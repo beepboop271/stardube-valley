d61f5010116146940b27b79e960c05da0d885314
Emerald <44278515+EmeraldEntities@users.noreply.github.com>
2019-12-23 13:01:15 -0500

    Update FarmArea.java

--- src/FarmArea.java
+++ src/FarmArea.java
@@ -2,15 +2,17 @@
  * [FarmArea]
  * 2019-12-17
  * @version 0.1
- * @author Kevin Qiao
+ * @author Kevin Qiao, Joseph Wang
  */
 public class FarmArea extends Area {
   // private FarmBuilding[] buildings;
-
+  //Hashset<> = new Hashset<>();
   public FarmArea(String name,
                   int width, int height) {
     super(name, width, height);
   }
+
+
 
   @Override
   public void doDayEndActions() {
