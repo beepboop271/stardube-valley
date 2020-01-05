2e8ae7b3901bfbcd071cf887b2976356eefa6a4b
beepboop271 <53926222+beepboop271@users.noreply.github.com>
2020-01-05 01:00:23 -0500

    maps and map converter script

---
+++ assets/maps/map_maker.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+COLOURS = {
+    (211, 163, 71 ): ".",
+    (21 , 255, 0  ): "x",
+    (0  , 255, 203): "o"
+}
+
+img = pygame.image.load("oceanArea.png")
+
+with open("Ocean", "w") as f:
+    for y in range(img.get_height()):
+        line = []
+        for x in range(img.get_width()):
+            clr = img.get_at((x, y))
+            if clr.a > 0:
+                line.append(COLOURS.get((clr.r, clr.g, clr.b)))
+            else:
+                line.append(" ")
+        f.write("".join(line)+"\n")
