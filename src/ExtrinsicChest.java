5ad0745b840165e63e78cc773cabc58ee7ecec1b
Emerald <44278515+EmeraldEntities@users.noreply.github.com>
2020-01-08 22:30:26 -0500

    a new challenger approaches: cauliflower
    added cauliflower
    also made axe image
    also started work on chests code
    also made a bridge in farm

---
+++ src/ExtrinsicChest.java
@@ -0,0 +1,13 @@
+// public class ExtrinsicChest extends ExtrinsicTileComponent {
+//   //- IntrinsicChest is just IntrinsicHarvestableComponent
+//   public ExtrinsicChest() {
+//     super("Chest");
+
+    
+//   }
+
+//   @Override
+//   public HoldableDrop[] getProducts() {
+
+//   }
+// }
