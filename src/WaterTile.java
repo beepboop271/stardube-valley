2ccdc5e6d237c7b5b5b51ad21be9a1b7a1e08887
Candice Zhang <58599808+candicezzzzz@users.noreply.github.com>
2020-01-16 14:32:40 -0400

    ++kevin

--- src/WaterTile.java
+++ src/WaterTile.java
@@ -32,7 +32,7 @@ public abstract class WaterTile extends Tile {
   }
   
   public static void setFishableTrash() {
-    WaterTile.fishableTrash = new String[] {"Joseph's-Compsci-Mark", "Purple-Algae", "Ethics-Assignment", "Chair-NULL", "MakBooc"};
+    WaterTile.fishableTrash = new String[] {"Joseph's-Compsci-Mark", "Purple-Algae", "Kevin's-Ethics-Assignment", "Chair-NULL", "MakBooc"};
   }
 
   public static String[] getFishableTrash() {
