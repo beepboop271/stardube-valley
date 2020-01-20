f80cf622a23c69ed6dca3f751819af0faaab52ef
Emerald <44278515+EmeraldEntities@users.noreply.github.com>
2020-01-20 11:22:19 -0500

    more javadocs
    leaving english also goodbye timer

--- src/TimedHoldableStack.java
+++ src/TimedHoldableStack.java
@@ -1,13 +1,21 @@
 /**
  * [TimedHoldableStack]
+ * A holdable stack that also stores a time needed for it, used
+ * during producion.
  * 2020-01-13
  * @version 0.1
  * @author Joseph Wang
  */
 
-
-public class TimedHoldableStack extends HoldableStack { //TODO: JAVADOCS
+public class TimedHoldableStack extends HoldableStack {
   private long timeNeeded;
+  /**
+   * [TimedHoldableStack]
+   * Constructor for a new TimedHoldableStack.
+   * @param quantity        The quantity of the HoldableStack.
+   * @param holdableToDrop  The holdable to drop.
+   * @param timeNeeded      The time needed for this production.
+   */
   public TimedHoldableStack(int quantity, String holdableToDrop, long timeNeeded) {
     super(holdableToDrop, quantity);
 
@@ -17,7 +25,7 @@ public class TimedHoldableStack extends HoldableStack { //TODO: JAVADOCS
   /** 
    * [getTimeNeeded]
    * Gets the total amount of time this HoldableDrop needs to wait before being produced.
-   * @return a long with the total amount of time needed
+   * @return long, the total amount of time needed
    */
   public long getTimeNeeded() {
     return this.timeNeeded;
--- src/Useable.java
+++ src/Useable.java
@@ -1,6 +1,22 @@
 import java.io.IOException;
 
-public abstract class Useable extends Holdable { //TODO: JAVADOCS
+/**
+ * [Useable]
+ * Anything that can be used by the player to do something.
+ * 2019-12-20
+ * @version 0.2
+ * @author Kevin Qiao
+ */
+
+public abstract class Useable extends Holdable {
+  /**
+   * [Useable]
+   * Constructor for a new Usable.
+   * @param name         The name of the useable.
+   * @param description  The description of the useable.
+   * @param imagePath    The path to this useable's images.
+   * @throws IOException
+   */
   public Useable(String name, String description, String imagePath) throws IOException {
     super(name, description, imagePath);
   }
