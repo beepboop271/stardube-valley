2a068f48002a4092d0392755e02e684bc079fc8b
Candice Zhang <58599808+candicezzzzz@users.noreply.github.com>
2020-01-20 14:49:53 -0400

    Update NPC.java

--- src/NPC.java
+++ src/NPC.java
@@ -3,19 +3,22 @@ import java.io.IOException;
 /**
  * [NPC]
  * 2020-01-19
- * 
+ * A LoopAnimatedMoveable to represent an NPC in the game.
  * @version 0.1
  * @author Paula Yuan, Candice Zhang
  */
 
-public class NPC extends LoopAnimatedMoveable { //TODO: JAVADOCS
+public class NPC extends LoopAnimatedMoveable {
   private static final double MAX_SPEED = 1;
   public static final double SIZE = 0.35;
   private int index;
   private String name;
-  private String[] dialogueRotation = new String[5]; // possibly change size?
+  private String[] dialogueRotation = new String[5];
   private String profileDescription;
 
+  /**
+   * [NPC]
+   */
   public NPC(Point position, String name, int index,
             String[] dialogueRotation, String profileDescription) throws IOException {
     super(position, SIZE, "npcs/"+name, LoopAnimatedMoveable.WALKSTEP_FRAMES);
